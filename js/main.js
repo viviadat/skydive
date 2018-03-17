c26158175260ac7e778d7a43ac7c83df993647f5
viviadat <35119232+viviadat@users.noreply.github.com>
2018-03-16 18:12:14 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,29 +8,26 @@ window.onscroll = function(){
     fromTop = window.pageYOffset;
     console.log(fromTop);
 
-    counter.style.top = 500 + fromTop + "px";
-    chute.style.top = 200 + fromTop + "px";
 
-    if (fromTop < 3900){
+    if (fromTop < 3800){
       counter.style.backgroundImage="url(img/diver1.png)";
+      counter.style.backgroundRepeat = "no-repeat";
+      counter.style.top = 500 + fromTop + "px";
       counter.style.animation ="paradown 2s infinite"
       // document.getElementById('chute').style.display = 'none';
     }else {
       // document.getElementById('counter').style.display = 'none';
-      chute.style.backgroundImage ="url(img/diver2.png)";
-      chute.style.animation ="paradown 6s infinite";
+      counter.style.backgroundImage ="url(img/diver2.png)";
+      counter.style.animation ="paradown 6s infinite";
+      counter.style.top = 200 + fromTop + "px";
+      // counter.style.left = 10 + fromTop + "px";
     }if (fromTop > 8900){
       // chute.style.top = 100 + fromTop + "px";
       document.getElementById('counter').style.visibility = 'hidden';
       document.getElementById('chute').style.visibility = 'hidden';
       stand.style.animation ="lastdiver 3s once" }
-    // }
-      // stand.style.backgroundImage="url(img/diver3.png)";
-    // }if (fromTop > 9400){
-    //   document.getElementById('counter').style.display = 'none';
-    //   document.getElementById('chute').style.display = 'none';
-    // }
 
 }
+
 
 AOS.init(1200);
